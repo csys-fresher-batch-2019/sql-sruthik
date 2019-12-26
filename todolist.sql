4832ff0379fea893c6a037e44bbb4e3f04a78145
SRUTHI K <sruthik2020@srishakthi.ac.in>
2019-12-26 14:37:51 +0530

    Update todolist.sql

--- todolist.sql
+++ todolist.sql
@@ -1,19 +1,15 @@
-create table todolist{
-task_number number,
-task_name varchar2(20) not null,
-task_performer varchar2(20) not null,
-created_date date not null,
-modified_date date not null,
-task_priority number,
-task_deadline date not null,
-task_status char(2);
-constraint task_number_pk primary key(task_number);
-constraint task_status_ck check(task_status in('completed','notcompleted');
-constraint task_deadline_ck check(modified_date < task_deadline);   
-constraint task_priority_uq unique(task_priority);                                
-};
-insert into todolist(task_number,task_name,task_performer,created_date,modified_date,task_priority,task_deadline,task_status) values(1,'Open chrome','Sruthi',to_date('15-12-2019','dd-mm-yyyy'),to_date('17-12-2019','dd-mm-yyyy'),1,to_date('18-12-2019','dd-mm-yyyy'),'completed');
-insert into todolist(task_number,task_name,task_performer,created_date,modified_date,task_priority,task_deadline,task_status) values(2,'Open gmail','Sruthi',to_date('17-12-2019','dd-mm-yyyy'),to_date('19-12-2019','dd-mm-yyyy'),2,to_date('20-12-2019','dd-mm-yyyy'),'notcompleted');
+create table todolist2(
+task_no number,
+task_name varchar(50),
+deadline date,
+completed_on date,
+modified_on date,
+task_priority number
+);
+insert into todolist2(task_no,task_name,deadline,completed_on,modified_on,task_priority) values (1,'Send request',to_date('23-12-2019','dd-MM-yyyy'),to_date('22-12-2019','dd-MM-yyyy'),to_date('21-12-2019','dd-MM-yyyy'),1);
+insert into todolist2(task_no,task_name,deadline,completed_on,modified_on,task_priority) values (2,'receive request',to_date('23-12-2019','dd-MM-yyyy'),to_date('22-12-2019','dd-MM-yyyy'),to_date('21-12-2019','dd-MM-yyyy'),2);
+insert into todolist2(task_no,task_name,deadline,completed_on,modified_on,task_priority) values (3,'confirm request',to_date('23-12-2019','dd-MM-yyyy'),to_date('22-12-2019','dd-MM-yyyy'),to_date('21-12-2019','dd-MM-yyyy'),3);
+insert into todolist2(task_no,task_name,deadline,completed_on,modified_on,task_priority) values (4,'Send mail',to_date('23-12-2019','dd-MM-yyyy'),to_date('22-12-2019','dd-MM-yyyy'),to_date('21-12-2019','dd-MM-yyyy'),4);
+insert into todolist2(task_no,task_name,deadline,completed_on,modified_on,task_priority) values (5,'Send request',to_date('23-12-2019','dd-MM-yyyy'),to_date('22-12-2019','dd-MM-yyyy'),to_date('21-12-2019','dd-MM-yyyy'),5);
 
-select * from todolist where(task_status = 'completed') and order by(priority);
-
+select * from todolist2 order by task_priority;
