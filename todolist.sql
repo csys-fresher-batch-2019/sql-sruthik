437a77845a052dfd0e2442d42a6b9481b66c097b
SRUTHI K <sruthik2020@srishakthi.ac.in>
2019-12-26 11:47:53 +0530

    Update todolist.sql

--- todolist.sql
+++ todolist.sql
@@ -1,5 +1,5 @@
 create table todolist{
-task_number number not null,
+task_number number,
 task_name varchar2(20) not null,
 task_performer varchar2(20) not null,
 created_date date not null,
@@ -7,13 +7,13 @@ modified_date date not null,
 task_priority number,
 task_deadline date not null,
 task_status char(2);
-constraint task_id_pk primary key(task_id);
+constraint task_number_pk primary key(task_number);
 constraint task_status_ck check(task_status in('completed','notcompleted');
 constraint task_deadline_ck check(modified_date < task_deadline);   
 constraint task_priority_uq unique(task_priority);                                
 };
-insert into todolist(task_number,task_name,task_performer,created_date,modified_date,task_priority,task_deadline,task_status) values(1,'Open chrome','Sruthi',to_date('15-12-2019','dd-mm-yyyy'),to_date('17-12-2019','dd-mm-yyyy'),to_date('18-12-2019','dd-mm-yyyy'),'completed');
-insert into todolist(task_number,task_name,task_performer,created_date,modified_date,task_priority,task_deadline,task_status) values(2,'Open gmail','Sruthi',to_date('17-12-2019','dd-mm-yyyy'),to_date('19-12-2019','dd-mm-yyyy'),to_date('20-12-2019','dd-mm-yyyy'),'notcompleted');
+insert into todolist(task_number,task_name,task_performer,created_date,modified_date,task_priority,task_deadline,task_status) values(1,'Open chrome','Sruthi',to_date('15-12-2019','dd-mm-yyyy'),to_date('17-12-2019','dd-mm-yyyy'),1,to_date('18-12-2019','dd-mm-yyyy'),'completed');
+insert into todolist(task_number,task_name,task_performer,created_date,modified_date,task_priority,task_deadline,task_status) values(2,'Open gmail','Sruthi',to_date('17-12-2019','dd-mm-yyyy'),to_date('19-12-2019','dd-mm-yyyy'),2,to_date('20-12-2019','dd-mm-yyyy'),'notcompleted');
 
-select * from todolist where(task_status = 'completed');
+select * from todolist where(task_status = 'completed') and order by(priority);
 
