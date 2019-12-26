90d41dad01c6408381176eb96216623afb2abeab
SRUTHI K <sruthik2020@srishakthi.ac.in>
2019-12-26 11:41:48 +0530

    Update todolist.sql

--- todolist.sql
+++ todolist.sql
@@ -8,10 +8,12 @@ task_priority number,
 task_deadline date not null,
 task_status char(20);
 constraint task_id_pk primary key(task_id);
-constraint task_status_ck check(staus_check in('completed','notcompleted');
+constraint task_status_ck check(task_status in('completed','notcompleted');
+constraint task_deadline_ck check(modified_date < task_deadline);    
+                                
 };
-insert into todolist(task_number,task_name,task_performer,task_deadline,task_status) values(1,'Open chrome','Sruthi',to_date('18-12-2019','dd-mm-yyyy'),'completed');
-insert into todolist(task_number,task_name,task_performer,task_deadline,task_status) values(2,'Open gmail','Sruthi',to_date('19-12-2019','dd-mm-yyyy'),'notcompleted');
+insert into todolist(task_number,task_name,task_performer,created_date,modified_date,task_priority,task_deadline,task_status) values(1,'Open chrome','Sruthi',to_date('15-12-2019','dd-mm-yyyy'),to_date('17-12-2019','dd-mm-yyyy'),to_date('18-12-2019','dd-mm-yyyy'),'completed');
+insert into todolist(task_number,task_name,task_performer,created_date,modified_date,task_priority,task_deadline,task_status) values(2,'Open gmail','Sruthi',to_date('17-12-2019','dd-mm-yyyy'),to_date('19-12-2019','dd-mm-yyyy'),to_date('20-12-2019','dd-mm-yyyy'),'notcompleted');
 
 select * from todolist where(task_status = 'completed');
 
